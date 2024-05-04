d7ae4c0fa7e6952edef53fb5bbb9eda6f2328735
devheredia <devheredia@gmail.com>
2024-05-04 19:25:55 -0300

    feat: :zap: Adcionado bootstrap4
    Adcionei o bootstrap no projeto, pois utilizar o link pode ser prejudical, devido o fato que o link pode parar de funcionar, ter interferências. Mantendo assim ter o bootstrap no projeto mais seguro.

--- login/login_registro.php
+++ login/login_registro.php
@@ -1,3 +1,6 @@
+<?php 
+    include 'includes.php'; 
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
--- login/login_view.php
+++ login/login_view.php
@@ -20,8 +20,6 @@ if (isset($_GET['sucesso']) && $_GET['sucesso'] === "sucesso") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="css/login_view.css">
-    <link rel="stylesheet" href="../global/bootstrap4/css/bootstrap.min.css">
-    <script src="../global/bootstrap4/js/bootstrap.min.js"></script>
 </head>
 
 <body>

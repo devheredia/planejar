8fe61c61eb81224d89af169151fc538f03a4b1fa
devheredia <devheredia@gmail.com>
2024-05-05 11:27:50 -0300

    feat: :sparkles: Adcionando centralizados
    Adcionado a página de menu, também foi acrescentado o include no CSS único da home, e acrescentado o menu genérico

---
+++ home/includes.php
@@ -0,0 +1,10 @@
+<?php
+    include_once('../config.php');
+    include '../menu_lateral/menu_lateral.php';
+    include '../global/css/style.php';
+    include '../global/js/script.php';
+    include '../global/sweetAlert/sweetAlert.php';
+    include '../global/sweetAlert/sweetalert2.min.php';
+    include '../global/dataTable/includes.php';
+    include 'css/home.php';
+?>
